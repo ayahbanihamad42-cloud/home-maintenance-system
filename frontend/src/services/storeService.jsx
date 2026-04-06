e2dd673c23f2d6248f8208f9cb8b8f02084aa107
tuqa <tmalrawashdeh21@cit.just.edu.jo>
2026-04-06 12:39:23 -0700

    Refactor storeService to use updated API import

--- frontend/src/services/storeService.jsx
+++ frontend/src/services/storeService.jsx
@@ -1,6 +1,10 @@
-import API from "./api";
+ mobile/src/services/storeService.js
+
+import api from "./api";
 
 export const getStoresByService = async (service) => {
-  const res = await API.get(`/stores?service=${service}`);
+  const res = await api.get(`/stores?service=${service}`);
   return res.data;
 };
+
+
