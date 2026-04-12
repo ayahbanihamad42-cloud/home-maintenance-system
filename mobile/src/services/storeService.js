923e1b5fa445ea908bbab2b4d367863f7b1d11af
tuqa <tmalrawashdeh21@cit.just.edu.jo>
2026-04-12 09:42:05 -0700

    Update storeService.js

---
+++ mobile/src/services/storeService.js
@@ -0,0 +1,8 @@
+mobile/src/services/storeService.js
+
+import api from "./api";
+
+export const getStoresByService = async (service) => {
+  const res = await api.get(`/stores?service=${service}`);
+  return res.data;
+};
