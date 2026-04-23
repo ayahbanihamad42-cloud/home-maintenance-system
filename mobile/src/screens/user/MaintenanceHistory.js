e4312009e99523e571c22c50ac136cf1bc3e3d16
ayahbanihamad42-cloud <ayahbanihamad42@gmail.com>
2026-04-23 09:52:45 +0300

    Fix import statement formatting in MaintenanceHistory.js

--- mobile/src/screens/user/MaintenanceHistory.js
+++ mobile/src/screens/user/MaintenanceHistory.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react-native";
-import { View, Text, StyleSheet, FlatList, SafeAreaView } from 
-  ";
+import { View, Text, StyleSheet, FlatList, SafeAreaView } from " 
+ react-native ";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { getUserRequests } from "../../services/maintenanceService";
